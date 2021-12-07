4b0cf83b880b1c00db8eb1846bd67668ae1fd903
archit828 <garchit2000@gmail.com>
2021-12-07 19:54:06 +0530

    API for fetching user data

--- controllers/users.js
+++ controllers/users.js
@@ -32,3 +32,57 @@ exports.postAddUser = async (req, res, next) => {
     });
   }
 };
+
+////Getting user data from firestore
+exports.getUserData = async (req, res, next) => {
+  try {
+    const user = await firestore
+      .collection("users")
+      .doc(req.user.user_id)
+      .get();
+    if (!user.exists) {
+      return res.status(404).send({
+        message: "User data does not exists",
+      });
+    }
+    let userData = user.data();
+    userData.user_id = req.user.user_id;
+    res.send(userData).status(200);
+  } catch (err) {
+    res.status(500).send({
+      message: "Internal error occurred",
+      error: err,
+    });
+  }
+};
+
+////Editing user details
+exports.editUserData = async (req, res, next) => {
+  try {
+    const user = await firestore
+      .collection("users")
+      .doc(req.user.user_id)
+      .get();
+    if (!user.exists) {
+      return res.status(404).send({
+        message: "User data does not exists",
+      });
+    }
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send({
+        error: errors.array()[0].msg,
+      });
+    }
+    let data = req.body;
+    await firestore.collection("users").doc(req.user.user_id).set(data);
+    res.status(201).send({
+      message: "User data edited successfully",
+    });
+  } catch (err) {
+    res.status(500).send({
+      message: "Internal error occurred",
+      error: err,
+    });
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { check } = require("express-validator");
-const { postAddUser } = require("../controllers/users");
+const {
+  postAddUser,
+  getUserData,
+  editUserData,
+} = require("../controllers/users");
 const isAuth = require("../middleware/is-auth");
 
 ////POST route to add the user data to firestore
@@ -10,6 +14,17 @@ router.post(
   isAuth,
   check("firstName", "First name is required").isLength({ min: 2 }),
   postAddUser
+);
+
+////GET route to fetch user data from firestore
+router.get("/getUser", isAuth, getUserData);
+
+////POST route to edit user details in firestore
+router.post(
+  "/editUser",
+  isAuth,
+  check("firstName", "First name is required").isLength({ min: 2 }),
+  editUserData
 );
 
 module.exports = router;
